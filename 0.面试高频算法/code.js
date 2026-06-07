1e67df1849277f44100ce5b1ff6e6c06e1f7eab9
Thinkingreedi <2310268377@qq.com>
2026-06-07 23:30:55 +0800

    feat: 6.7

--- 0.面试高频算法/code.js
+++ 0.面试高频算法/code.js
@@ -493,7 +493,44 @@ function curry(fn) {
     };
 }
 // 手写Promise
-// ...
+class MyPromise {
+  // 构造方法
+  constructor(executor) {
+    // 初始化值
+    this.initValue()
+    // 初始化this指向
+    this.initBind()
+    // 执行传进来的函数
+    executor(this.resolve, this.reject)
+  }
+
+  initBind() {
+    // 初始化this
+    this.resolve = this.resolve.bind(this)
+    this.reject = this.reject.bind(this)
+  }
+
+  initValue() {
+    // 初始化值
+    this.PromiseResult = null // 终值
+    this.PromiseState = 'pending' // 状态
+  }
+
+  resolve(value) {
+    // 如果执行resolve，状态变为fulfilled
+    this.PromiseState = 'fulfilled'
+    // 终值为传进来的值
+    this.PromiseResult = value
+  }
+
+  reject(reason) {
+    // 如果执行reject，状态变为rejected
+    this.PromiseState = 'rejected'
+    // 终值为传进来的reason
+    this.PromiseResult = reason
+  }
+}
+
 // 手写浅拷贝
 function shallowCopy(object) {
     if (!object || typeof object !== "object")
